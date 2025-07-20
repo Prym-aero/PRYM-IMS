cb6d1ef4f3bfd40978c3d303134026f009a556eb
sarthak12005 <sarthakjoshi12005@gmail.com>
2025-07-20 11:42:26 +0530

    added some changes

--- ERP-frontend/src/components/InventoryCompnent.jsx
+++ ERP-frontend/src/components/InventoryCompnent.jsx
@@ -383,13 +383,6 @@ const InventoryDispatchSystem = () => {
             </div>
           </div>
 
-          {/* Help Button */}
-          <div className="fixed bottom-6 right-6">
-            <button className="bg-blue-600 text-white p-3 rounded-full hover:bg-blue-700 shadow-lg">
-              <HelpCircle className="w-6 h-6" />
-            </button>
-          </div>
-
           {dispatchStage === "scanning" && currentDispatchData && (
             <DispatchScanComponent
               dispatchData={currentDispatchData}
